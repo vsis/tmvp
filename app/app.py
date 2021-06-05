36133de738faff92287b59e5a26db5b1e4877b9c
Roddy González <roddy.gonzalez.89@gmail.com>
2021-06-05 17:46:13 -0400

    Fix messages in app.py

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER = '/path/to/the/uploads'
+UPLOAD_FOLDER = '/tmp/'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 @app.route("/", methods=['GET'])
@@ -13,27 +13,31 @@ def index():
 
 @app.route("/img", methods=['POST'])
 def img():
+
+    # Check if filename has an allowed extension
     def allowed(filename):
         try:
             return filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
+        # This exception will happen if filename has no dot
         except IndexError:
-            return false
+            return False
+
     returned_data = {'message': ''}
     status = 200
     try:
         file = request.files['file']
         filename = secure_filename(file.filename)
         if allowed(filename):
-            file.save(filename)
-            returned_data = {'message': 'file ok'}
+            file.save(UPLOAD_FOLDER + filename)
+            returned_data = {'message': 'File OK'}
         else:
-            returned_data = {'message': 'file not allowed'}
+            returned_data = {'message': 'File not allowed. Use one of these extensions: ' + str(ALLOWED_EXTENSIONS)}
             status = 400
     except KeyError:
-        returned_data = {'message': "didn't you give a file?"}
+        returned_data = {'message': "No file was given."}
         status = 400
     except Exception as error:
-        returned_data = {'message': 'I have no idea what happened', 'error': str(error)}
+        returned_data = {'message': 'There was a server error =(', 'error': str(error)}
         status = 500
     return jsonify(returned_data), status
 
